097025cc8b93de71d10043365830dde43b69488a
Damien White <damienkwhite@gmail.com>
2023-06-19 20:56:54 +0100

    added photos, random button and some styling

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const randomNumber = () => (
-     Math.floor(Math.random() * 6) +1
-)
+const img = document.querySelector("img");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+       let randomNumber = Math.floor(Math.random() * 18) + 1;
+        img.src = `./images/${randomNumber}.jpeg`;
+    console.log("img")
+});
+
+
+
